ebd249c994ffcad91fd59307cc1518ad46c9adfc
Vitor Souza Amaral <vitoramaral2001.bkbd@gmail.com>
2022-05-01 12:31:21 -0300

    Projeto Criado

---
+++ src/test/java/com/vitoramaral/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitoramaral.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
